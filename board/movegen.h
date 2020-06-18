9af6eed6465cdf4a4bf46489c49a443b60aaaabd
Chris <knightchris2001@yahoo.com>
2020-06-17 22:11:02 -0400

    Bugfix type

--- board/movegen.h
+++ board/movegen.h
@@ -12,7 +12,7 @@ public:
 	using BoardType = Board;
 	using MoveType = typename BoardType::MoveType;
 
-	NodeCount generateAllMoves(Board& board, MoveList<MoveType>& moveList, bool countOnly = false)
+	NodeCount generateAllMoves(BoardType& board, MoveList<MoveType>& moveList, bool countOnly = false)
 	{
 		return static_cast<T*>(this)->generateAllMovesImplementation(board, moveList, countOnly);
 	}
