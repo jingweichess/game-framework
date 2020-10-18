6a5f00fad160eaa6cf041389d1b8a8b15026bb3d
chrisflorin <knightchris2001@yahoo.com>
2020-10-18 18:06:57 -0400

    Add negation function

--- types/color.h
+++ types/color.h
@@ -9,3 +9,13 @@ static Color& operator ++ (Color& c, int)
 {
     return c = Color(int(c) + 1);
 }
+
+static Color operator ~ (Color& c)
+{
+    if (c == Color::WHITE) {
+        return Color::BLACK;
+    }
+    else {
+        return Color::WHITE;
+    }
+}
